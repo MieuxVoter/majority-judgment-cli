de3098bfe2cce25a21505bcbc435303a3753afc2
domi41 <dominique.merle.41@gmail.com>
2021-10-27 11:27:13 +0200

    test: add a rudimentary benchmark

---
+++ benchmark/README.md
@@ -0,0 +1,9 @@
+
+> Very rudimentary, for now.  Add more!
+
+You're welcome to upload your results with your CPU, especially if they differ from ours.
+
+## Benchmarking
+
+    go test benchmark/*.go -bench=.
+
---
+++ benchmark/loops_test.go
@@ -0,0 +1,79 @@
+package benchmark
+
+// from https://github.com/piersy/iterate-slice-bench-go
+// with fixes (methink)
+
+import (
+	"testing"
+)
+
+var Val = uint64(42)
+var slice = make([]uint64, 500*500)
+
+func BenchmarkRangeReadSliceByIndex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j, _ := range slice {
+			Val = slice[j] + 1
+		}
+	}
+}
+
+func BenchmarkRangeReadSliceByValue(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, v := range slice {
+			Val = v + 1
+		}
+	}
+}
+
+func BenchmarkRangeWriteSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j, _ := range slice {
+			slice[j] = Val + 1
+		}
+	}
+}
+
+func BenchmarkRangeReadAndWriteSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j, v := range slice {
+			slice[j] = v + 1
+		}
+	}
+}
+
+func BenchmarkForIterReadSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		l := len(slice)
+		for j := 0; j < l; j++ {
+			Val = slice[j] + 1
+		}
+	}
+}
+
+func BenchmarkForIterWriteSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		l := len(slice)
+		for j := 0; j < l; j++ {
+			slice[j] = Val + 1
+		}
+	}
+}
+
+func BenchmarkForIterReadAndWriteSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		l := len(slice)
+		for j := 0; j < l; j++ {
+			slice[j] = slice[j] + 1
+		}
+	}
+}
+
+func BenchmarkDecreasingForIterReadAndWriteSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		l := len(slice)
+		for j := l - 1; j >= 0; j-- {
+			slice[j] = slice[j] + 1
+		}
+	}
+}
