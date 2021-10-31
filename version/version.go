6a1db3d0902a1012162fcf4fc14c7274be4f239d
domi41 <dominique.merle.41@gmail.com>
2021-10-31 20:15:36 +0100

    docs: explain the version package's purpose

--- version/version.go
+++ version/version.go
@@ -1,6 +1,8 @@
 package version
 
-// GitSummary is filled through -ldflags upon building
+// These properties are filled through -ldflags upon building using govvv
+
+// GitSummary is a long, descriptive git version like 0.3.1 or 0.3.1-12-g3257b77
 var GitSummary string
 
 // BuildDate looks like this 2021-10-20T12:24:58Z
