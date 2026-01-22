c1653e8f341186d9fd0b2549b2a1b1fe92addf72
domi41 <dominique.merle.41@gmail.com>
2026-01-22 07:58:36 +0100

    config: review the build script
    OMG this is minimalistic :p
    We'll redo the release pipeline when we connect it to the CI.

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@
 #apt install upx
 #go get github.com/ahmetb/govvv
 
-go build \
+GOOS=linux GOARCH=amd64 go build \
+  -v \
   -ldflags="$(govvv -flags -pkg $(go list ./version)) -s -w" \
   -o mj
 
 GOOS=windows GOARCH=amd64 go build \
+  -v \
   -ldflags="$(govvv -flags -pkg $(go list ./version)) -s -w" \
   -o mj.exe
 
 upx mj
-
